f1ebe11b2dc27e446c84b86167481dba7a7472b6
Vishaldrapper <33867147+Vishaldrapper@users.noreply.github.com>
2019-07-30 23:06:30 +0530

    Update dividestring.py
    Divide a string by another string and display the remaining string

--- dividestring.py
+++ dividestring.py
@@ -1,7 +1,9 @@
+#given a string as abcabcvtabc
+#divide this string by another string e.g ab
+#output should be ccvtc
 if __name__ == "__main__":
     str1 = "abcabcvtabc"
     str2 = "ab"
-    # op should be "de"
     if str1.__contains__(str2):
         x = str1.replace(str2,"")
         print(x)
